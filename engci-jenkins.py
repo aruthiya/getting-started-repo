f8670656b055e6a9737a9e6a56fee53fa44dfb04
aruthiya <aruthiya@cisco.com>
2017-02-13 14:04:18 +0530

    Update engci-jenkins.py

--- engci-jenkins.py
+++ engci-jenkins.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import jenkins
+import os
 
-
-
+print'We are going inside jenkins'
 server = jenkins.Jenkins('https://engci-private-blr.cisco.com/jenkins/ethan-blr/job/HIP/', username='aruthiya', password='David12#$')
 
 # Create a new job
