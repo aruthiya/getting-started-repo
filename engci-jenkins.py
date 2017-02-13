1b0d4fa9d230784cec011f170eabb2b35e13ab24
aruthiya <aruthiya@cisco.com>
2017-02-13 14:08:27 +0530

    Update engci-jenkins.py

--- engci-jenkins.py
+++ engci-jenkins.py
@@ -4,7 +4,5 @@ import os
 
 print('We are going inside jenkins')
 server = jenkins.Jenkins('https://engci-private-blr.cisco.com/jenkins/ethan-blr/job/HIP/', username='aruthiya', password='David12#$')
+print('We logged in jenkins')
 
-# Create a new job
-if not server.job_exists('empty'):
-    server.create_job('empty', jenkins.EMPTY_CONFIG_XML)
