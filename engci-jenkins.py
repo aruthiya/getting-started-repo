c2161ab918d19c5dbd292bbb2eff2739f17c24f5
aruthiya <aruthiya@cisco.com>
2017-02-13 12:01:36 +0530

    Update engci-jenkins.py

--- engci-jenkins.py
+++ engci-jenkins.py
@@ -5,7 +5,9 @@ import json
 import random
 
 
-server = jenkins.Jenkins('https://engci-jenkins-rtp.cisco.com/jenkins/', username='aruthiya', password='A1b2c3d4$%^&')
-user = server.get_whoami()
-version = server.get_version()
-print('Hello %s from Jenkins %s' % (user['fullName'], version))
+server = jenkins.Jenkins('https://engci-private-blr.cisco.com/jenkins/ethan-blr/job/HIP/', username='aruthiya', password='A1b2c3d4$%^&')
+
+# Create a new job
+if not server.job_exists('empty_hip'):
+    server.create_job('empty_hip', jenkins.EMPTY_CONFIG_XML)
+server.disable_job('empty_hip')
