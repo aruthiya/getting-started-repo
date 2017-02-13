1593d118ec616c77c3d096829cced2168896b572
aruthiya <aruthiya@cisco.com>
2017-02-13 12:29:57 +0530

    Update engci-jenkins.py

--- engci-jenkins.py
+++ engci-jenkins.py
@@ -7,7 +7,4 @@ import random
 
 server = jenkins.Jenkins('https://engci-private-blr.cisco.com/jenkins/ethan-blr/job/HIP/', username='aruthiya', password='A1b2c3d4$%^&')
 
-# Create a new job
-if not server.job_exists('empty_hip'):
-    server.create_job('empty_hip', jenkins.EMPTY_CONFIG_XML)
-server.disable_job('empty_hip')
+
