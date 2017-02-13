e022d7358ebf7d62f24d486886cb99503cce24be
aruthiya <aruthiya@cisco.com>
2017-02-13 15:40:02 +0530

    Update engci-jenkins.py

--- engci-jenkins.py
+++ engci-jenkins.py
@@ -6,16 +6,16 @@ server = jenkins.Jenkins('https://engci-private-blr.cisco.com/jenkins/ethan-blr/
 print('We logged in jenkins')
 
 # Copy a job
-print('We are going to copy the existing job')
-if server.job_exists('testjob'):
-   server.copy_job('testjob', 'testjob_1')
-print('We are going to enable job')
-server.enable_job('testjob_1')
+#print('We are going to copy the existing job')
+#if server.job_exists('testjob'):
+#   server.copy_job('testjob', 'testjob_1')
+#print('We are going to enable job')
+#server.enable_job('testjob_1')
 
 #jobs = server.get_jobs()
 #print jobs
 
 # trigger job
-#print('We are going to trigger a empty job')
-#server.build_job('empty_copy', parameters=None, token='ecea623de363e1f3068890231772bf8b')
+print('We are going to trigger a empty job')
+server.build_job('empty_copy', parameters=None, token='ecea623de363e1f3068890231772bf8b')
 print('We are done')
