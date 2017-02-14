c28ddc7d80c7153c6e59b89edd5acc70694d832e
aruthiya <aruthiya@cisco.com>
2017-02-14 11:55:33 +0530

    Update engci-jenkins.py

--- engci-jenkins.py
+++ engci-jenkins.py
@@ -1,21 +1,41 @@
 #!/usr/bin/python
-import jenkins
 
-print('We are going inside jenkins')
-server = jenkins.Jenkins('https://engci-jenkins-rtp.cisco.com/jenkins/job/team_spvtg_carina/job/Debug/job/B/', username='aruthiya', password='David12#$')
-print('We logged in jenkins')
+import json 
+import sys
+import urllib2
+import base64
 
-# Copy a job
-#print('We are going to copy the existing job')
-#if server.job_exists('testjob'):
-#   server.copy_job('testjob', 'testjob_1')
-#print('We are going to enable job')
-#server.enable_job('testjob_1')
+user = aruthiya
+password = David12#$
+jenkinsUrl = https://engci-jenkins-rtp.cisco.com/jenkins/job/team_spvtg_carina/job/Debug/job/B/
+jobName = Metadata_SFS_POC
 
-#jobs = server.get_jobs()
-#print jobs
+def urlopen(url, data=None):
+    '''Open a URL using the urllib2 opener.'''
+    request = urllib2.Request(url, data)
+    base64string = base64.encodestring('%s:%s' % (user, password)).replace('\n', '')
+    request.add_header("Authorization", "Basic %s" % base64string)
+    response = urllib2.urlopen(request)
+    return response
 
-# trigger job
-print('We are going to trigger a empty job')
-server.build_job('Metadata_SFS_POC', parameters=None, token='eb6ccb5583324aef25ef605bb8c05c8a')
-print('We are done')
+try:
+    jenkinsStream   = urlopen(jenkinsUrl + jobName + "/lastBuild/api/json")
+except urllib2.HTTPError, e:
+    print "URL Error: " + str(e.code) 
+    print "      (job name [" + jobName + "] probably wrong)"
+    sys.exit(2)
+
+try:
+    buildStatusJson = json.load( jenkinsStream )
+except:
+    print "Failed to parse json"
+    sys.exit(3)
+
+if buildStatusJson.has_key( "result" ):      
+    print "[" + jobName + " #" + str(buildStatusJson["number"]) + "]: " + buildStatusJson["result"] 
+    if buildStatusJson["result"] != "SUCCESS" :
+		exit(4)
+else:
+	sys.exit(5)
+
+sys.exit(0)
