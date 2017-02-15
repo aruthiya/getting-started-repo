bde5c99dec86ff78b8bf14644a23b2d9ebe60215
aruthiya <aruthiya@cisco.com>
2017-02-15 15:22:40 +0530

    Update Example_2.py

--- Example_2.py
+++ Example_2.py
@@ -2,13 +2,13 @@
 import jenkins
 
 print('We are going inside jenkins')
-server = jenkins.Jenkins('https://engci-jenkins-rtp.cisco.com/jenkins/job/team_spvtg_carina/job/Debug/job/B/', username='aruthiya', password='David12#$')
+server = jenkins.Jenkins('https://engci-private-blr.cisco.com/jenkins/ethan-blr/job/HIP/', username='aruthiya', password='David12#$')
 print('We logged in jenkins')
 
 # Copy a job
 print('We are going to copy the existing job')
-if server.job_exists('Metadata_SFS_POC'):
-   server.copy_job('Metadata_SFS_POC', 'Metadata_SFS_POC_1')
+if server.job_exists('testjob'):
+   server.copy_job('testjob', 'Metadata_SFS_POC_1')
 print('We are going to enable job')
 server.enable_job('Metadata_SFS_POC_1')
 
@@ -17,5 +17,5 @@ server.enable_job('Metadata_SFS_POC_1')
 
 # trigger job
 print('We are going to trigger a empty job')
-server.build_job('Metadata_SFS_POC_1', parameters=None, token='eb6ccb5583324aef25ef605bb8c05c8a')
+server.build_job('Metadata_SFS_POC_1', parameters=None, token='ecea623de363e1f3068890231772bf8b')
 print('We are done')
